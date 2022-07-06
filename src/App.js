431a8d68f53a4c7d3956814b14eb6e265624222a
DaSom253 <idasom253@gmail.com>
2022-07-06 19:53:17 +0900

    feat : home에서 카드 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Basket from "./pages/Basket";
 import './App.css';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      프로젝트 시작
-    </div>
+    <BrowserRouter basename="comento-shopping">
+      <Routes>     
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/product/:productId" element = {<ProductDetail />}/>
+        <Route path = "/basket" element = {<Basket />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

d1ec3f00a663fca55619d4b86b6c908054039c8b
DaSom253 <idasom253@gmail.com>
2022-07-06 20:48:49 +0900

    feat : components에 styled-components 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
+import styled from "styled-components";
 
 const Home = () => {
     return (
       <div>
-        <Navigation />
-
         <div>
-          <ThemeButton themeName={"#겨울 방한템"}/>
-          <ThemeButton themeName={"#따순머그컵"}/>
+          <Navigation />
+          <ThemeSection>
+            <ThemeButton themeName={"#겨울 방한템"}/>
+            <ThemeButton themeName={"#따순 머그컵"}/>
+          </ThemeSection>
+
+          <GrayLine />
         </div>
 
         <div>
+          <ProductSection>
           <ProductCard 
           name="비숑 블랙 머그잔" 
           description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 
@@ -31,12 +36,29 @@ const Home = () => {
           name="벨루즈까사 솜사탕 파스텔 머그 4종 세트" 
           description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 
 더해져 심플하면서도 세련된 파스텔 컬러의 머그" 
-          humbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
-          />
+          thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
+          />         
+          </ProductSection>
         </div>
         
        </div>
     );
   };
+
+  const ThemeSection = styled.div`
+  display: flex;
+  gap: 12px;
+  padding: 40px 12px;
+  `;
+
+  const GrayLine = styled.div`
+  height: 8px;
+  width: 100%;
+  background-color: #eeeeee;
+  `;
+
+  const ProductSection = styled.div`
+  padding: 20px;
+  `;
   
   export default Home;
